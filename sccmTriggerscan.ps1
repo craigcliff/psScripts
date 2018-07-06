d227f5dd5202d5dd904d525b5bbf899bfbf4d7ba
Craig <craigcliff@gmail.com>
2018-07-06 09:05:17 +0200

    added sccm Trigger scan

---
+++ sccmTriggerscan.ps1
@@ -0,0 +1,25 @@
+﻿$workstations =  'DST00011'
+
+
+
+foreach($workstation in $workstations) {
+
+   if (-not (Test-Connection -comp $workstation -quiet)){
+                Write-host "unable to ping $workstation" -ForegroundColor Red
+            } Else {
+
+
+
+
+#Machine Policy Retrieval
+Invoke-WMIMethod -ComputerName $workstation -Namespace root\ccm -Class SMS_CLIENT -Name TriggerSchedule "{00000000-0000-0000-0000-000000000021}"
+
+#Machine Policy Evaluation Cycle
+Invoke-WMIMethod -ComputerName $workstation -Namespace root\ccm -Class SMS_CLIENT -Name TriggerSchedule "{00000000-0000-0000-0000-000000000022}"
+
+#Application Deployment Evaluation Cycle
+Invoke-WMIMethod -ComputerName $workstation -Namespace root\ccm -Class SMS_CLIENT -Name TriggerSchedule "{00000000-0000-0000-0000-000000000121}"
+}
+
+
+}
